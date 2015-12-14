1832d02a35e8efb813592d1ac23b6562d7f132ed
wolf-alex-shishkin <wolf.alex.shishkin@gmail.com>
2015-12-15 00:18:13 +0500

    Заготовка класса бота и тестов для него

---
+++ botworld.bl.tests/BotTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using botworld.bl;
+using System;
+
+namespace botworld.bl.tests
+{
+    [TestFixture]
+    public class BotTests
+    {
+        [Test]
+        public void Name_Returns_NameSetByConstructor()
+        {
+            var bot = new Bot("Angry bot");
+
+            Assert.That(bot.Name, Is.EqualTo("Angry bot"));
+        }
+    }
+}
---
+++ botworld.bl/Bot.cs
@@ -0,0 +1,12 @@
+﻿namespace botworld.bl
+{
+    public class Bot
+    {
+        public string Name { get; private set; }
+
+        public Bot(string name)
+        {
+            Name = name;
+        }
+    }
+}
