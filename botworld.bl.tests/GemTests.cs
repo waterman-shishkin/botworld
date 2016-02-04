36bdcc801f373f5507210a49ac594c1d336fdb28
Eugene Shishkin <eugene.shishkin@skbkontur.ru>
2016-02-04 10:24:50 +0500

    WP (win points) вместо Points в IPointsProvider

--- botworld.bl.tests/GemTests.cs
+++ botworld.bl.tests/GemTests.cs
@@ -14,11 +14,11 @@ namespace botworld.bl.tests
 		}
 
 		[Test]
-		public void Points_Returns_PointsSetByConstructor()
+		public void WP_Returns_PointsSetByConstructor()
 		{
 			var gem = new Gem(100, new Location(2, 4));
 
-			Assert.That(gem.Points, Is.EqualTo(100));
+			Assert.That(gem.WP, Is.EqualTo(100));
 		}
 
 		[Test]
--- botworld.bl/Gem.cs
+++ botworld.bl/Gem.cs
@@ -2,10 +2,10 @@ namespace botworld.bl
 {
 	public class Gem : IEntity, IPointsProvider
 	{
-		public Gem(int points, Location location)
+		public Gem(int wp, Location location)
 		{
 			HP = 1;
-			Points = points;
+			WP = wp;
 			Location = location;
 		}
 
@@ -37,7 +37,7 @@ namespace botworld.bl
 
 		public double DefenceStrength { get { return 0; } }
 
-		public int Points { get; private set; }
+		public int WP { get; private set; }
 
 		public bool ImpactDamage(double damage)
 		{
