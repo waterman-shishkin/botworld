411ddba03defdc80fce3a58d016aba1a1631a5b9
Eugene Shishkin <eugene.shishkin@skbkontur.ru>
2016-03-02 23:31:18 +0500

    Еще порция функциональных тестов для Game

--- botworld.bl.tests/GameFuncTests.cs
+++ botworld.bl.tests/GameFuncTests.cs
@@ -267,11 +267,72 @@ namespace botworld.bl.tests
 			Assert.That(bot.Location, Is.EqualTo(targetLocation));
 		}
 
-		// Act - атака на пустую клетку
-		// Act - атака на клетку за границей
-		// Act - атака на клетку, где не отвечают на атаку
-		// Act - атака на клетку, где отвечают на атаку
-		// Act - атака на клетку, где отвечают на атаку и погибаем
+		[Test]
+		public void Tick_ForAgressiveBotWhichDesireToMakeStepToCellOccupiedByTwoAgressiveBots_ResultsInGuestBotDamageAndMovementAndHostBotsDamage()
+		{
+			var guestBotIntelligence = Substitute.For<IBotIntelligence>();
+			guestBotIntelligence.ChooseNextAction(Arg.Any<BotInfo>(), Arg.Any<Dictionary<Location, IEnumerable<EntityInfo>>>()).ReturnsForAnyArgs(BotAction.Step);
+			guestBotIntelligence.ChooseAttackResponseAction(Arg.Any<BotInfo>(), Arg.Any<EntityInfo>()).ReturnsForAnyArgs(AttackResponseAction.Attack);
+			var guestBot = new Bot("Guest", 100, 10, 0, 5, new Location(2, 4), Direction.North, guestBotIntelligence);
+			var hostBotIntelligence = Substitute.For<IBotIntelligence>();
+			hostBotIntelligence.ChooseNextAction(Arg.Any<BotInfo>(), Arg.Any<Dictionary<Location, IEnumerable<EntityInfo>>>()).ReturnsForAnyArgs(BotAction.Step);
+			hostBotIntelligence.ChooseInvasionResponseAction(Arg.Any<BotInfo>(), Arg.Any<EntityInfo>()).ReturnsForAnyArgs(InvasionResponseAction.Attack);
+			var targetLocation = new Location(2, 3);
+			var hostBot1 = new Bot("Host-1", 90, 10, 0, 5, targetLocation, Direction.West, hostBotIntelligence);
+			var hostBot2 = new Bot("Host-2", 80, 10, 0, 5, targetLocation, Direction.East, hostBotIntelligence);
+			var map = new Map(10, 20);
+			map.Add(guestBot);
+			map.Add(hostBot1);
+			map.Add(hostBot2);
+			var scenario = Substitute.For<IGameScenario>();
+			var game = new Game(map, scenario);
+
+			game.Tick();
+
+			Assert.That(hostBot1.HP, Is.EqualTo(85));
+			Assert.That(hostBot1.Location, Is.EqualTo(new Location(1, 3)));
+			Assert.That(hostBot2.HP, Is.EqualTo(75));
+			Assert.That(hostBot2.Location, Is.EqualTo(new Location(3, 3)));
+			Assert.That(guestBot.HP, Is.EqualTo(90));
+			Assert.That(guestBot.Location, Is.EqualTo(targetLocation));
+		}
+
+		[Test]
+		public void Tick_ForBotWhichDesireToActOnEmptyCell_DoNotThrow()
+		{
+			var botIntelligence = Substitute.For<IBotIntelligence>();
+			botIntelligence.ChooseNextAction(Arg.Any<BotInfo>(), Arg.Any<Dictionary<Location, IEnumerable<EntityInfo>>>()).ReturnsForAnyArgs(BotAction.Act);
+			var bot = new Bot("Bot", 20, 10, 0, 5, new Location(2, 4), Direction.North, botIntelligence);
+			var map = new Map(10, 20);
+			map.Add(bot);
+			var scenario = Substitute.For<IGameScenario>();
+			var game = new Game(map, scenario);
+
+			TestDelegate testDelegate = () => game.Tick();
+
+			Assert.DoesNotThrow(testDelegate);
+		}
+
+		[Test]
+		public void Tick_ForBotWhichDesireToActOutsideOfMap_DoNotThrow()
+		{
+			var botIntelligence = Substitute.For<IBotIntelligence>();
+			botIntelligence.ChooseNextAction(Arg.Any<BotInfo>(), Arg.Any<Dictionary<Location, IEnumerable<EntityInfo>>>()).ReturnsForAnyArgs(BotAction.Act);
+			var bot = new Bot("Bot", 20, 10, 0, 5, new Location(0, 0), Direction.North, botIntelligence);
+			var map = new Map(10, 20);
+			map.Add(bot);
+			var scenario = Substitute.For<IGameScenario>();
+			var game = new Game(map, scenario);
+
+			TestDelegate testDelegate = () => game.Tick();
+
+			Assert.DoesNotThrow(testDelegate);
+		}
+
+		// Act - атака на клетку, где не отвечают на атаку (стена (слабая и сильная), мина, гем)
+		// Act - атака на клетку, где отвечают на атаку (бот)
+		// Act - атака на клетку, где отвечают на атаку много кто (несколько ботов, слабый и сильный)
+		// Act - атака на клетку, где отвечают на атаку и погибаем (бот)
 		// Collect - когда нечего собирать
 		// Collect - собираем то, что можно собрать
 		// Explore - исследование клетки
